166ca9475c9518142c1ee93b54d1eb45e62d1788
Andrea Amaya <andreaa.malin@gmail.com>
2022-05-09 23:44:27 -0600

    Arreglado el thread id

--- monitor.cpp
+++ monitor.cpp
@@ -26,58 +26,50 @@ class Monitor{
         pthread_mutex_t  MonitorLock;
 
     public:
-    
         void counterInit()
         {
             pthread_mutex_init(&MonitorLock, NULL);
         }
 
-    
         void counterEnd()
         {
             pthread_mutex_destroy(&MonitorLock);
         }
 
-        void decrease()
-        {
-            printf("entraaaa\n");
-            file << "Iniciando decrease_count\n";
-            file << "Mutex adquirido, entrando al monitor\n";
-
-            if (MAX_RESOURCES >= RESOURCES_PER_THREAD) {
-                file << "Recusos suficientes disponibles, consumiendo... \n";
-                pthread_mutex_lock(&MonitorLock);
-                for (int i = 0; i < RESOURCES_PER_THREAD; i++) {
-                    MAX_RESOURCES--;
-                }
-                pthread_mutex_unlock(&MonitorLock);
-            } else {
-                file << "NUAY! Recursos actuales: " << MAX_RESOURCES << "\n";
-            }
-            file << "Terminando decrease_count\n" ;
-        }
+        void* decrease();
 };
 
-Monitor monitor;
+void* Monitor::decrease() {
+    printf("entraaaa\n");
+    pthread_mutex_lock(&MonitorLock);
+    file << "Iniciando Threads " << syscall(SYS_gettid) << "\n";
+    
+    file << "Iniciando decrease_count\n";
+    file << "Mutex adquirido, entrando al monitor\n";
 
-void* threadWork(void* args) {
-    int threadID = syscall(SYS_gettid);
-    file << threadID << " - Iniciando iteracion " << ITERATIONS << "\n";
+    if (MAX_RESOURCES >= RESOURCES_PER_THREAD) {
+        file << "Recusos suficientes disponibles, consumiendo... \n";
+        for (int i = 0; i < RESOURCES_PER_THREAD; i++) {
+            MAX_RESOURCES--;
+        }
+    } else {
+        file << "NUAY! Recursos actuales: " << MAX_RESOURCES << "\n";
+    }
+    file << "Terminando decrease_count\n" ;
+    pthread_mutex_unlock(&MonitorLock);
 }
 
+typedef void* (*THREADFUNCMONITOR) (void *);
+
 int main() {
+    Monitor* monitor = new Monitor();
     file.open("./BitacoraSemaforo2.txt", ofstream::out);
-    atomic<int> aux (0); 
     file << "--- Iniciando el programa ---" << "\n";
     pthread_t threadPool[THREAD_POOL];
-    monitor.counterInit();
 
-    int mainThreadID = syscall(SYS_gettid);
     file << "Creando Threads" << "\n";
     for (int i = 0; i < THREAD_POOL; i++) {
-        file << "Iniciando Threads " << syscall(SYS_gettid) << "\n";
-        pthread_create(&threadPool[i], NULL, threadWork, &file);
-        monitor.decrease();
+        pthread_create(&threadPool[i], NULL, (THREADFUNCMONITOR)&Monitor::decrease, monitor);
         ITERATIONS++;
     }
 
@@ -85,7 +77,6 @@ int main() {
         pthread_join(threadPool[i], NULL);
     }
 
-    monitor.counterEnd();
     file << "--- Terminando programa ---" << "\n";
     file.close();
     return 0;
